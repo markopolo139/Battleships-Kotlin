072d52593aea338c34c2e6388e053b13e5bdc6f5
maropolo139 <marekseget@onet.pl>
2022-06-03 11:52:39 +0200

    feat(build): add jwt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,10 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
 	implementation("org.passay:passay:1.6.1")
 
+	implementation("io.jsonwebtoken:jjwt-api:0.11.1")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.1")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+
 	implementation("org.springframework.boot:spring-boot-starter-log4j2")
 	configurations {
 		all{
