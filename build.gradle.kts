117740e10afd886f4d96803458af815db7242995
maropolo139 <marekseget@onet.pl>
2022-06-12 21:06:39 +0200

    fix(build): add missing security dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,27 +27,33 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.security:spring-security-messaging:5.7.1")
+
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("org.passay:passay:1.6.1")
+
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-	implementation("org.passay:passay:1.6.1")
 
-	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
 	implementation("org.springframework.boot:spring-boot-starter-log4j2")
 	configurations {
