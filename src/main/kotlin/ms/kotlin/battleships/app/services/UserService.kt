626bcc482ca9fdcaac48e87d7ed6243e2f3a8236
maropolo139 <marekseget@onet.pl>
2022-06-12 21:07:02 +0200

    fix(UserService): null check when assigning userId

--- src/main/kotlin/ms/kotlin/battleships/app/services/UserService.kt
+++ src/main/kotlin/ms/kotlin/battleships/app/services/UserService.kt
@@ -17,7 +17,7 @@ class UserService: UserDetailsService {
     @Autowired
     private lateinit var  userRepository: UserRepository
 
-    private val userId = (SecurityContextHolder.getContext().authentication as AppUserEntity).id
+    private val userId = (SecurityContextHolder.getContext().authentication as? AppUserEntity)?.id ?: 0
 
     override fun loadUserByUsername(username: String?): UserDetails {
         if (username == null)
