bedd6ecaf2faaa96842c4981acd97d9ba4724120
maropolo139 <marekseget@onet.pl>
2022-06-15 14:36:53 +0200

    feat(testController): add websocket mapping

--- src/main/kotlin/ms/kotlin/battleships/web/TestController.kt
+++ src/main/kotlin/ms/kotlin/battleships/web/TestController.kt
@@ -1,7 +1,12 @@
 package ms.kotlin.battleships.web
 
+import ms.kotlin.battleships.app.security.AppUserEntity
 import ms.kotlin.battleships.app.services.AuthService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.simp.annotation.SendToUser
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
@@ -27,6 +32,12 @@ class TestController {
     @GetMapping("/api/v1/test")
     fun testToken(): String {
         return "Ok"
+    }
+
+    @MessageMapping("/make/shot")
+    @SendTo(value = ["/topic/ships"])
+    fun testWS(): Int {
+        return (SecurityContextHolder.getContext().authentication.principal as? AppUserEntity)?.id ?: 0
     }
 
 }
